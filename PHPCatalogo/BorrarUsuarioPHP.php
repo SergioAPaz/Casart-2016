3eca3983c2cfeb70313da4e04abf7d674bdde890
SergioAPaz <alejandroax@live.com.mx>
2016-07-13 18:05:48 -0600

    Borrado de usuarios: el sistema debe contar con por lo menos 1 administrador.

--- PHPCatalogo/BorrarUsuarioPHP.php
+++ PHPCatalogo/BorrarUsuarioPHP.php
@@ -7,14 +7,54 @@ if(isset($_GET['id']))
     include('conexion.php');
 
 
+    /*CONSULTA PARA LEER ROL*/
+    $queryRol = <<<SQL
+SELECT Rol FROM `users` WHERE id='$id'
+SQL;
+    $execute = mysqli_query($conexiondb,$queryRol);
+    $Rol = mysqli_fetch_array($execute);
+
+
+/*CONSULTA PARA CONTAR EL NUMERO DE ADMINISTRADORES DEL SISTEMA*/
+    $ContadorAdmins=<<<SQL
+SELECT id FROM `users` WHERE Rol='Administrador'
+SQL;
+    $cuenta=mysqli_query($conexiondb,$ContadorAdmins);
+    $total = mysqli_num_rows($cuenta);
+
+
+if($Rol['Rol']=="Administrador" )
+{
+            if ($total>1)
+            {
+                $consulta=<<<SQL
+DELETE  FROM users WHERE id='$id'
+SQL;
+                mysqli_query($conexiondb,$consulta);
+
+                header("Location: ../NuevosUsuarios.php");
+
+            }
+            else
+            {
+                  echo "Imposible eliminar, el sistema debe contar con por lo menos un usuario administrador.";
+            }
+} else if ($Rol['Rol']=="Usuario") {
+
     $consulta=<<<SQL
 DELETE  FROM users WHERE id='$id'
 SQL;
-
-
     mysqli_query($conexiondb,$consulta);
+
+    header("Location: ../NuevosUsuarios.php");
 
 }
 
-header("Location: ../NuevosUsuarios.php");
+
+}else{
+    echo "Fallo al eliminar";
+}
+
+
+
 ?>
